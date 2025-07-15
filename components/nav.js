d5a41bb3fbcb029bb52c8b3474227046fe3f7e4d
MOHDAHMEDKHAN27052003 <mohd.ahmedkhan190@gmail.com>
2025-07-15 20:51:22 +0530

    navbar styled

--- components/nav.js
+++ components/nav.js
@@ -3,9 +3,11 @@
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { Menu, X } from 'lucide-react';
 
 export default function Navbar() {
   const [user, setUser] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
   const pathname = usePathname();
 
   useEffect(() => {
@@ -13,62 +15,119 @@ export default function Navbar() {
     setUser(storedUser);
   }, []);
 
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+
   // Utility to check if link is active
   const linkClass = (href) => {
     const isActive = pathname === href;
-    return `${isActive ? 'bg-blue-600 text-white' : ''}`;
+    return `${isActive ? 'text-blue-600' : ''}`;
   };
 
   return (
-    <nav
-    >
-      <Link href="/">
-        📚 BOOKTRACK
-      </Link>
+    <>
+      <nav className='flex items-center justify-around p-4'>
+        <Link href="/" className='text-2xl font-extralight'>
+          📚 BOOKTRACK
+        </Link>
 
-      <div>
-        {user?.role === 'admin' && (
-          <>
-            <Link href="/admin/issued" className={linkClass('/admin/issued')}>
-              Issued Books
-            </Link>
-            <Link href="/books/create" className={linkClass("/books/create")}>
-              Create Book
-            </Link>
-            <Link href="/profile" className={linkClass("/profile")}>
-              Profile
-            </Link>
-          </>
-        )}
+        <button
+          onClick={toggleMenu}
+          className="md:hidden"
+        >
+          {isOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
 
-        {user?.role === 'user' && (
-          <>
-            <Link href="/" className={linkClass('/')}>
-              Home
-            </Link>
-            <Link href="/books/issued" className={linkClass("/books/issued")}>
-              Issued Books
-            </Link>
-            <Link href="/profile" className={linkClass("/profile")}>
-              Profile
-            </Link>
-          </>
-        )}
+        <div className="hidden md:flex bg-black text-white px-24 py-4 rounded-lg">
+          {user?.role === 'admin' && (
+            <div className='flex gap-16'>
+              <Link href="/admin/issued" className={linkClass('/admin/issued')}>
+                Issued Books
+              </Link>
+              <Link href="/books/create" className={linkClass("/books/create")}>
+                Create Book
+              </Link>
+              <Link href="/profile" className={linkClass("/profile")}>
+                Profile
+              </Link>
+            </div>
+          )}
 
-        {!user && (
-          <>
-            <Link href="/" className={linkClass('/')}>
-              Home
-            </Link>
-            <Link href="/signin" className={linkClass("/signin")}>
-              SignIn
-            </Link>
-            <Link href="/signup" className={linkClass("/signup")}>
-              SignUp
-            </Link>
-          </>
-        )}
+          {user?.role === 'user' && (
+            <div className='flex gap-16'>
+              <Link href="/" className={linkClass('/')}>
+                Home
+              </Link>
+              <Link href="/books/issued" className={linkClass("/books/issued")}>
+                Issued Books
+              </Link>
+              <Link href="/profile" className={linkClass("/profile")}>
+                Profile
+              </Link>
+            </div>
+          )}
+
+          {!user && (
+            <div className='flex gap-16'>
+              <Link href="/" className={linkClass('/')}>
+                Home
+              </Link>
+              <Link href="/signin" className={linkClass("/signin")}>
+                Sign In
+              </Link>
+              <Link href="/signup" className={linkClass("/signup")}>
+                Sign Up
+              </Link>
+            </div>
+          )}
+        </div>
+      </nav>
+      <div className={`md:hidden transition-all duration-300 ease-in-out transform overflow-hidden ${
+          isOpen ? 'max-h-[500px] opacity-100 scale-100' : 'max-h-0 opacity-0 scale-95'
+        } px-4 sm:px-16`}>
+        <div className="flex flex-col gap-4 py-8 items-center rounded-2xl bg-black text-white">
+          {user?.role === 'admin' && (
+            <>
+              <Link href="/admin/issued" className={linkClass('/admin/issued')}>
+                Issued Books
+              </Link>
+              <Link href="/books/create" className={linkClass('/books/create')}>
+                Create Book
+              </Link>
+              <Link href="/profile" className={linkClass('/profile')}>
+                Profile
+              </Link>
+            </>
+          )}
+
+          {user?.role === 'user' && (
+            <>
+              <Link href="/" className={linkClass('/')}>
+                Home
+              </Link>
+              <Link href="/books/issued" className={linkClass('/books/issued')}>
+                Issued Books
+              </Link>
+              <Link href="/profile" className={linkClass('/profile')}>
+                Profile
+              </Link>
+            </>
+          )}
+
+          {!user && (
+            <>
+              <Link href="/" className={linkClass('/')}>
+                Home
+              </Link>
+              <Link href="/signin" className={linkClass('/signin')}>
+                Sign In
+              </Link>
+              <Link href="/signup" className={linkClass('/signup')}>
+                Sign Up
+              </Link>
+            </>
+          )}
+        </div>
       </div>
-    </nav>
+    </>
   );
 };
