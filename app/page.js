dd5719e12651a5cf553341f9f668a82772bd482b
MOHDAHMEDKHAN27052003 <mohd.ahmedkhan190@gmail.com>
2025-07-18 20:31:19 +0530

    home style optimized

--- app/page.js
+++ app/page.js
@@ -57,85 +57,88 @@ export default function Home() {
           </p>
         </div>
       )}
-      <div className='px-16 py-4 sm:px-24'>
-      <div className='flex flex-col sm:flex-row items-center gap-4 pt-8 pb-12'>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={search}
-          onChange={(e) => {
-            setSearch(e.target.value);
-            setVisibleCount(6); // Reset on search
-          }}
-          className='px-4 py-2 rounded-lg bg-green-400 text-white outline-0'
-        />
-
-        <select
-          value={genre}
-          onChange={(e) => {
-            setGenre(e.target.value);
-            setVisibleCount(6); // Reset on genre change
-          }}
-          className='p-2 rounded-lg bg-green-400 text-white outline-0'
-        >
-          {genres.map((g, i) => (
-            <option
-              key={i}
-              value={g}
-            >
-              {g}
-            </option>
-          ))}
-        </select>
-
-        <button
-          onClick={() => {
-            setSearch('');
-            setGenre('All');
-            setVisibleCount(6);
-          }}
-          className='bg-red-600 text-white px-6 py-2 rounded-lg cursor-pointer'
-        >
-          Reset
-        </button>  
-      </div>
-
-      {visibleBooks.length === 0 ? (
-        <p>No books found.</p>
-      ) : (
-        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 sm:gap-16'>
-          {visibleBooks.map((book) => (
-            <div
-              key={book.id}
-              onClick={() => { router.push(`/books/${book.id}`) }}
-              className='flex flex-col items-center gap-2 py-16 lg:py-20 bg-gray-800 text-white rounded-2xl'
-            >
-              <Image
-                src={book.image}
-                alt={book.title}
-                height={0}
-                width={0}
-                className='rounded-xl h-28 w-48'
-              />
-              <h4>{book.title}</h4>
-              <p>{book.author}</p>
-              <p>{book.genre}</p>
-            </div>
-          ))}
-        </div>
-      )}
-      {hasMore ? (   
-        <div className='flex justify-center py-16'>
-          <button
-            onClick={handleLoadMore}
-            className='bg-green-400 text-white px-4 py-2 rounded-lg cursor-pointer'
-          >
-            Load More
-          </button>
-        </div>
+      <div>
+        {visibleBooks.length === 0 ? (
+          <p>No books found.</p>
         ) : (
-        <p className='text-center py-8 text-lg'>You've reached the end of the list!</p>
-      )}
+          <div>
+            <div className='flex flex-col sm:flex-row justify-center items-center gap-4 pt-8 pb-12'>
+              <input
+                type="text"
+                placeholder="Search..."
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                  setVisibleCount(6); // Reset on search
+                }}
+                className='px-4 py-2 rounded-lg bg-green-400 text-white outline-0'
+              />
+
+              <select
+                value={genre}
+                onChange={(e) => {
+                  setGenre(e.target.value);
+                  setVisibleCount(6); // Reset on genre change
+                }}
+                className='p-2 rounded-lg bg-green-400 text-white outline-0'
+              >
+                {genres.map((g, i) => (
+                  <option
+                    key={i}
+                    value={g}
+                  >
+                    {g}
+                  </option>
+                ))}
+              </select>
+
+              <button
+                onClick={() => {
+                  setSearch('');
+                  setGenre('All');
+                  setVisibleCount(6);
+                }}
+                className='bg-red-600 text-white px-6 py-2 rounded-lg cursor-pointer'
+              >
+                Reset
+              </button>  
+            </div>
+            <div className='flex justify-center'>  
+              <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 sm:gap-16'>
+                {visibleBooks.map((book) => (
+                  <div
+                    key={book.id}
+                    onClick={() => { router.push(`/books/${book.id}`) }}
+                    className='flex flex-col items-center gap-2 px-6 py-16 lg:py-20 bg-gray-800 text-white rounded-2xl'
+                  >
+                    <Image
+                      src={book.image}
+                      alt={book.title}
+                      height={0}
+                      width={0}
+                      className='rounded-xl h-40 w-60'
+                    />
+                    <h4>{book.title}</h4>
+                    <p>{book.author}</p>
+                    <p>{book.genre}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+        {hasMore ? (   
+          <div className='flex justify-center py-16'>
+            <button
+              onClick={handleLoadMore}
+              className='bg-green-400 text-white px-4 py-2 rounded-lg cursor-pointer'
+            >
+              Load More
+            </button>
+          </div>
+        ) : (
+            <p className='text-center py-8 text-lg'>You've reached the end of the list!</p>
+        )}
       </div>
     </div>
   );
