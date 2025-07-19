872a7d6aea172a89def54eb6abb3ff401d141004
MOHDAHMEDKHAN27052003 <mohd.ahmedkhan190@gmail.com>
2025-07-19 14:09:44 +0530

    search filters fixed

--- app/page.js
+++ app/page.js
@@ -58,51 +58,49 @@ export default function Home() {
         </div>
       )}
       <div>
+        <div className='flex flex-col sm:flex-row justify-center items-center gap-4 pt-8 pb-12'>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={search}
+            onChange={(e) => {
+              setSearch(e.target.value);
+              setVisibleCount(6); // Reset on search
+            }}
+            className='px-4 py-2 rounded-lg bg-green-400 text-white outline-0'
+          />
+          <select
+            value={genre}
+            onChange={(e) => {
+              setGenre(e.target.value);
+              setVisibleCount(6); // Reset on genre change
+            }}
+            className='p-2 rounded-lg bg-green-400 text-white outline-0'
+          >
+            {genres.map((g, i) => (
+              <option
+                key={i}
+                value={g}
+              >
+                {g}
+              </option>
+            ))}
+          </select>
+          <button
+            onClick={() => {
+              setSearch('');
+              setGenre('All');
+              setVisibleCount(6);
+            }}
+            className='bg-red-600 text-white px-6 py-2 rounded-lg cursor-pointer'
+          >
+            Reset
+          </button>  
+        </div>
         {visibleBooks.length === 0 ? (
-          <p>No books found.</p>
+          <p className='text-center py-8 text-2xl'>No book found!</p>
         ) : (
           <div>
-            <div className='flex flex-col sm:flex-row justify-center items-center gap-4 pt-8 pb-12'>
-              <input
-                type="text"
-                placeholder="Search..."
-                value={search}
-                onChange={(e) => {
-                  setSearch(e.target.value);
-                  setVisibleCount(6); // Reset on search
-                }}
-                className='px-4 py-2 rounded-lg bg-green-400 text-white outline-0'
-              />
-
-              <select
-                value={genre}
-                onChange={(e) => {
-                  setGenre(e.target.value);
-                  setVisibleCount(6); // Reset on genre change
-                }}
-                className='p-2 rounded-lg bg-green-400 text-white outline-0'
-              >
-                {genres.map((g, i) => (
-                  <option
-                    key={i}
-                    value={g}
-                  >
-                    {g}
-                  </option>
-                ))}
-              </select>
-
-              <button
-                onClick={() => {
-                  setSearch('');
-                  setGenre('All');
-                  setVisibleCount(6);
-                }}
-                className='bg-red-600 text-white px-6 py-2 rounded-lg cursor-pointer'
-              >
-                Reset
-              </button>  
-            </div>
             <div className='flex justify-center'>  
               <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12 sm:gap-16'>
                 {visibleBooks.map((book) => (
@@ -137,7 +135,7 @@ export default function Home() {
             </button>
           </div>
         ) : (
-            <p className='text-center py-8 md:py-16 text-lg lg:text-xl'>You have reached the end of list.</p>
+          <p className='text-center py-8 md:py-16 text-lg lg:text-xl'>You have reached the end of list.</p>
         )}
       </div>
     </div>
